072829fdea152b6ba424785af1e0dc6ddc4e6e3b
GROOVETECH\cuonglam <cuong.lam@groovetechnology.com>
2018-10-17 13:49:11 +0700

    no message

---
+++ StudyAppCenter/StudyAppCenter/App.xaml.cs
@@ -0,0 +1,36 @@
+using System;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace StudyAppCenter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            AppCenter.Start("android=c41e3d3a-6c6a-4b70-9108-e589637b3a9d;", typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ StudyAppCenter/StudyAppCenter/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AppCenter.Analytics;
+using Xamarin.Forms;
+
+namespace StudyAppCenter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Analytics.TrackEvent("Video clicked", new Dictionary<string, string>()
+            {
+                { "Login", "aaa" },
+            });
+            InitializeComponent();
+        }
+    }
+}
